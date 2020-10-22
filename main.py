15a123cb7eb3b82d830a33afa9be68b3358cc718
Ty Chermsirivatana <professornitro@gmail.com>
2020-10-22 21:03:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# Python Logic Problem for Fun by Ty C
+# Note ^ = And and v = Or 
+p = True
+q = False
+r = True
+TFTrue = "is True"
+TFFalse = "is False"
+
+# Problem 1 
+if p == True and q == True:
+  print(f"Problem 1 {TFTrue}") 
+else: 
+  print(f"Problem 1 {TFFalse}")
+# Problem 2 
+if q == True and r == True: 
+  print(f"Problem 2 {TFTrue}")
+else: 
+  print(f"Problem 2 {TFFalse}")
+# Problem 3 
+if p == True and r == True: 
+  print(f"Problem 3 {TFTrue}")
+else: 
+  print(f"Problem 3 {TFFalse}")
+# Problem 4 
+if p == False and q == True:
+  print(f"Problem 4 {TFTrue}")
+else: 
+  print(f"Problem 4 {TFFalse}")
+# Pen or Pencil Regent Problem
+pen = True
+pencil = True 
+
+if pen == True or pencil == True: 
+  print("You are ready for the regents!")
+else: 
+  print("You need more preparation")
+ 
+
